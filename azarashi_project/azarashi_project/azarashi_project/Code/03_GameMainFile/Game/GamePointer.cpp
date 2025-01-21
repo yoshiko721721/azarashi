a35cb5c6adab37175b259fd575ba0f576e470c66
KodoKE2004 <kouta159635@icloud.com>
2025-01-21 13:14:25 +0900

    rigidbodyの更新＋α
    GameSceneでPointerの当たり判定処理を施していたものをPointer自身のUpdate内で行えるようになりました。

--- azarashi_project/azarashi_project/azarashi_project/Code/03_GameMainFile/Game/GamePointer.cpp
+++ azarashi_project/azarashi_project/azarashi_project/Code/03_GameMainFile/Game/GamePointer.cpp
@@ -1,7 +1,7 @@
 #include "GamePointer.h"
 #include "../../03_GameMainFile/Math.h"
 
-using DirectX::XMFLOAT3 ;
+using DirectX::XMFLOAT3;
 
 //=========================================
 //				初期化処理
@@ -9,13 +9,12 @@ using DirectX::XMFLOAT3 ;
 void GamePointer::Init()
 {
 	Initialize(AZARASHI_PICTURE_STAND);   //背景を初期化
-	SetPos(   0.0f,   0.0f, 0.0f);		  //位置を設定
+	SetPos(0.0f, 0.0f, 0.0f);		  //位置を設定
 	SetSize(100.0f, 100.0f, 0.0f);		  //大きさを設定
 	SetAngle(0.0f);						  //角度を設定
 	SetColor(1.0f, 1.0f, 1.0f, 1.0f);	  //色を設定
 
 	circle.radius = GetPos().y / 2;
-	
 
 	body.SetMass(7.0f);					  //質量を設定
 	body.SetTime(0.01);					  //時間を初期化
@@ -28,20 +27,60 @@ void GamePointer::Init()
 //				更新処理					
 //===========================================
 
-void GamePointer::Update(float blockAngle)//Playerのアップデート
+void GamePointer::Update(ContactPointVector collision, Object& block)//Playerのアップデート
 {
-	Vector2 pos = { GetPos().x,GetPos().y };
 
-	body.TimeCounter(FRAMERATE);
-	body.Update(pos, body.GetTime());		//時間を引数にして物理の更新処理
+	Vector2 blockPos = { block.GetPos().x, block.GetPos().y };
+	now = collision.checkCollision;
 
-	
+	//自由落下
+	body.FreeFall(body.GetTime());
+
+	//衝突しているの処理
+	if (now)
+	{
+
+		body.CalcFainalNormalAngle(collision.closspoint, *this, block);	//法線の角度を計算
+		body.HorizonUpdate(block, AZARASHI_MODE[azaNum]);			//転がる処理
+
+		//ジャンプ
+		if (Input::GetKeyTrigger(VK_RETURN)) {
+			body.AddForce(0.0f, 15.0f);
+		}
 
 
-	pos.x += body.GetVector().x;			//
-	pos.y += body.GetVector().y;			//最後に位置を移動した分更新
 
-	SetPos(pos.x, pos.y, 0);
+		body.DampingVector(damping, azaNum);
+		body.Repulsion();			//反発
+
+		//座標を補正
+		GamePointer::CorrectPosition(&block, collision.closspoint,
+			collision.DistanceSquared, block.GetAngle());
+
+	}
+
+
+	//モード切り替え
+	if (Input::GetKeyPress(VK_T)) {
+		SetAzaNum(CIRCLE);
+	}
+	else {
+		SetAzaNum(STAND);
+	}
+
+	//モードチェンジ
+	if (isChangeMode()) {
+		switch (azaNum) {
+		case CIRCLE: Initialize(AZARASHI_PICTURE_CIRCLE); break;
+		case STAND:  Initialize(AZARASHI_PICTURE_STAND);
+			SetAngle(block.GetAngle());			  break;
+		}
+	}
+
+	//当たり判定の記録
+	old = now;
+	SetPos(GetPos().x + body.GetVector().x, GetPos().y + body.GetVector().y, 0);
+
 }
 
 float GamePointer::GetCircleRadius()
@@ -60,9 +99,9 @@ void GamePointer::RotateTexture(const float sub)  //引数　：　当たった物体の摩擦
 	float tempAngle = GetAngle();
 
 	if (body.GetVector().x < 0)
-		tempAngle -= body.GetVector().x + AZARASHI_MODE[azaNum] ;
+		tempAngle -= body.GetVector().x + AZARASHI_MODE[azaNum];
 	else if (body.GetVector().x > 0)
-		tempAngle -= body.GetVector().x + AZARASHI_MODE[azaNum] ;
+		tempAngle -= body.GetVector().x + AZARASHI_MODE[azaNum];
 
 	tempAngle -= body.vector.x;
 
@@ -81,5 +120,22 @@ void GamePointer::SetAzaNum(AZA_MODE_NUMMBER m_AzaNum)
 {
 	oldAzaNum = azaNum;
 	azaNum = m_AzaNum;
+}
+
+/// @brief 座標の補正
+/// @param m_Block	当たったブロックの引数
+/// @param clossPoint 円と四角形の接地点
+/// @param distanceSquared 円の中心座標とclossPointの距離の2乗
+/// @param angle blockの角度
+void GamePointer::CorrectPosition(Object* m_Block, XMFLOAT2 clossPoint, float distanceSquared, float angle)
+{
+	float distance = sqrt(distanceSquared);				//正式な距離にする
+	float overlap = (GetSize().y / 2) - distance;		//円の半径
+	XMFLOAT2 direction = { GetPos().x - clossPoint.x, GetPos().y - clossPoint.y };
+	float length = sqrt(direction.x * direction.x + direction.y * direction.y);
+	direction = { direction.x / length, direction.y / length };
+
+
+	SetPos(GetPos().x + direction.x * overlap, GetPos().y + direction.y * overlap, 0);
 }
 
--- azarashi_project/azarashi_project/azarashi_project/Code/03_GameMainFile/Game/GamePointer.h
+++ azarashi_project/azarashi_project/azarashi_project/Code/03_GameMainFile/Game/GamePointer.h
@@ -2,16 +2,14 @@
 #include "../../01_Direct3D/Object.h"
 #include "../../02_Input/input.h"
 #include "../../03_GameMainFile/RigidBody.h"
+#include"../../08_Collider/Base_collision.h"
 #include "GameBlock.h"
 
 #define AZARASHI_PICTURE_CIRCLE L"Asset/pic/azarasi.png"
-#define AZARASHI_PICTURE_STAND  L"Asset/pic/MatsuFace_transparent.png"
-
-
+#define AZARASHI_PICTURE_STAND  L"Asset/pic/point.png"
 
 struct Circle
 {
-	//float x, y; // 位置
 	float radius; // 半径
 	float velocityX; // 垂直方向の速度
 	float velocityY; // 垂直方向の速度
@@ -25,28 +23,27 @@ public:
 	GamePointer() {}
 	~GamePointer() { Uninit(); }
 	void Init();  //初期化
-	void Update(float blockAngle);//更新
+	void Update(ContactPointVector contactPointVector, Object& block);//更新
 
 
 	float GetCircleRadius();		//半径のゲッター
 	float GetFrictionResistance();	//摩擦抵抗のゲッター
-	
+
 	void  RotateTexture(const float sub);					//画像を丁度よく回転
 	AZA_MODE_NUMMBER azaNum = STAND;
 	AZA_MODE_NUMMBER oldAzaNum = azaNum;
 
-
 	bool isChangeMode();									//アザラシのモード比較
 	void SetAzaNum(AZA_MODE_NUMMBER m_azaNum);				//アザラシの画像セット
 
-
-	//float GetCirclePosX();
-	//float GetCirclePosY();
+	void CorrectPosition(Object* m_Block, XMFLOAT2 clossPoint, float distanceSquared, float angle);		//座標の補正
 
 	float damping = 0.9f;
 private:
 	Circle circle;
 	const float AZARASHI_MODE[MODENUM] = { 0.3 , 0.2 };	// 摩擦係数
+
+	bool old = false;
+	bool now = false;
 	//const float gravity = -0.4f;					// 重力の力
 };
-
